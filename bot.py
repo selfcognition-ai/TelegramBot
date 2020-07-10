1e892c9faab03b70b919099d3b5535801f6e95fd
Mirakuru <selfcognition@yandex.ru>
2020-07-10 14:03:30 +0400

    определение геолокации и номера телефона

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from random import choice
 
 from emoji import emojize
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import mysettings
@@ -17,9 +17,7 @@ def greet_user(bot, update, user_data):
 	emo = get_user_emo(user_data)
 	user_data['emo'] = emo
 	text = 'Привет {}!'.format(emo)
-	my_keyboard = ReplyKeyboardMarkup([['/cat', '/change_avatar']],
-									  resize_keyboard=True)
-	update.message.reply_text(text, reply_markup=my_keyboard)
+	update.message.reply_text(text, reply_markup=get_keyboard())
 
 def talk_to_me(bot, update, user_data):
 	emo = get_user_emo(user_data)
@@ -28,19 +26,12 @@ def talk_to_me(bot, update, user_data):
 	logging.info("User: %s, Chat id: %s, Message: %s", 
 				 update.message.chat.username,
 				 update.message.chat.id, update.message.text)
-	update.message.reply_text(user_text)
+	update.message.reply_text(user_text, reply_markup=get_keyboard())
 
 def send_cat_picture(bot, update, user_data):
 	cat_list = glob('images/cat*.jp*g')
 	cat_pic = choice(cat_list)
-	bot.send_photo(chat_id=update.message.chat_id, photo=open(cat_pic, 'rb'))
-
-def get_user_emo(user_data):
-	if 'emo' in user_data:
-		return user_data['emo']
-	else:
-		user_data['emo'] = emojize(choice(mysettings.USER_EMOJI), use_aliases=True)
-		return user_data['emo']
+	bot.send_photo(chat_id=update.message.chat_id, photo=open(cat_pic, 'rb'), reply_markup=get_keyboard())
 
 def change_avatar(bot, update, user_data):	
 	emo = get_user_emo(user_data)	
@@ -51,8 +42,33 @@ def change_avatar(bot, update, user_data):
 		while emo == old_emo:
 			del user_data['emo']
 			emo = get_user_emo(user_data)			
-	update.message.reply_text('Ваш аватар: {}'.format(emo))
-		
+	update.message.reply_text('Ваш аватар: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
+
+def get_contact(bot, update, user_data):
+	print(update.message.contact)
+	update.message.reply_text('Готово: {}'.format(emo), reply_markup=get_keyboard())
+
+def get_location(bot, update, user_data):
+	print(update.message.location)
+	update.message.reply_text('Готово: {}'.format(emo), reply_markup=get_keyboard())
+
+def get_user_emo(user_data):
+	if 'emo' in user_data:
+		return user_data['emo']
+	else:
+		user_data['emo'] = emojize(choice(mysettings.USER_EMOJI), use_aliases=True)
+		return user_data['emo']
+
+def get_keyboard():
+	contact_button = KeyboardButton('Прислать свой номер', request_contact=True)
+	location_button = KeyboardButton('Прислать координаты', request_location=True)
+	my_keyboard = ReplyKeyboardMarkup(
+									  [['/cat', '/change_avatar'],
+									  [contact_button, location_button]
+									  ],
+									  resize_keyboard=True)
+	return my_keyboard
+
 def main():
 	mybot = Updater(mysettings.API_KEY)
 
@@ -60,6 +76,8 @@ def main():
 	dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
 	dp.add_handler(CommandHandler('cat', send_cat_picture, pass_user_data=True))
 	dp.add_handler(CommandHandler('change_avatar', change_avatar, pass_user_data=True))
+	dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+	dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
 	dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
 	logging.info('Starting bot')
