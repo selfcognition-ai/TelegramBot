0e032d546fbae7ca12ec3136a5a3ab72f73c73aa
Mirakuru <selfcognition@yandex.ru>
2020-07-18 20:21:58 +0400

    Добавлено получение фото от пользователей, и сохранение при наличие кота

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ def main():
 	dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
 	dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
 	dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
+	dp.add_handler(MessageHandler(Filters.photo, check_user_photo, pass_user_data=True))
 
 	logging.info('Starting bot')
 	mybot.start_polling()
--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
 from glob import glob
 import logging
+import os
 from random import choice
 
-from utils import get_keyboard, get_user_emo
+from utils import get_keyboard, get_user_emo, is_cat
 
 def greet_user(bot, update, user_data):
 	emo = get_user_emo(user_data)
@@ -42,3 +43,17 @@ def get_contact(bot, update, user_data):
 def get_location(bot, update, user_data):
 	print(update.message.location)
 	update.message.reply_text('Готово: {}'.format(emo), reply_markup=get_keyboard())
+
+def check_user_photo(bot, update, user_data):
+	update.message.reply_text('Идёт обработка фото')
+	os.makedirs('downloads', exist_ok=True)
+	photo_file = bot.getFile(update.message.photo[-1].file_id)
+	filename = os.path.join('downloads', '{}.jpg'.format(photo_file.file_id))
+	photo_file.download(filename)
+	if is_cat(filename):
+		update.message.reply_text('Котик замечен, добавлено в библиотеку')
+		new_filename = os.path.join('images', 'cat_{}.jpg'.format(photo_file.file_id))
+		os.rename(filename, new_filename)
+	else:
+		os.remove(filename)
+		update.message.reply_text('Котика не нашлось')
